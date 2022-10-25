8db19c222bacbabe9bb990847956215d1ace0d97
HHM-1999 <hhm639069@gmail.com>
2022-10-25 19:57:49 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const Port = process.env.PORT || 5000;
+const categories = require('./data/catagories.json');
 
 app.get('/', (req, res) => {
     res.send('API is running');
 });
 app.listen(Port, () => {
     console.log("Courses running in port", Port)
-})
+});
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+});
