f2c5b28327e27985b7e504c93a60673dde5a33a9
HHM-1999 <hhm639069@gmail.com>
2022-10-27 04:22:47 +0530

    6th commit

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ const cors = require('cors');
 const Port = process.env.PORT || 5000;
 const categories = require('./data/catagories.json');
 
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('API is running');
 });
-app.listen(Port, () => {
-    console.log("Courses running in port", Port)
-});
+
 app.get('/course-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/courses/:id', (req, res) => {
+    console.log(req.params);
+})
+app.listen(Port, () => {
+    console.log("Courses running in port", Port)
 });
